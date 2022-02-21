4c1e48ea9f46718ab44385efb7e4c880d883af91
dsgdops <82456829+dsgdops@users.noreply.github.com>
2022-02-21 17:35:44 +0100

    Class models

--- src/scan/models.py
+++ src/scan/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class scans(models.Model):
+    host = models.CharField(max_length=200)
+    ports = models.IntegerField()
+    date = models.DateField()
