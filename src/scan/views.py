d68ecb9ff841e7a7a61ca6698b79b5a297bddd0d
dsgdops <82456829+dsgdops@users.noreply.github.com>
2022-02-23 11:42:07 +0100

    run scan

--- src/scan/views.py
+++ src/scan/views.py
@@ -17,7 +17,6 @@ class IndexView(ListView):
 def add_report(request):
     q = scan(host="127.0.0.1", ports=script.run_scan("127.0.0.1"), date="2022-02-23")
     q.save()
-
     return HttpResponse("Oui")
 
 
