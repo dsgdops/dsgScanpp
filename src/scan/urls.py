77a19fbb91f51ca5edea3f1562f644711b3ea3bb
dsgdops <82456829+dsgdops@users.noreply.github.com>
2022-02-24 08:57:16 +0100

    url redirection sur  template

--- src/scan/urls.py
+++ src/scan/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('add/<str:ip_address>', views.add_report, name='addreport'),
+    path('', views.IndexView.as_view(), name='home-scan'),
+    path('add/<str:ip_address>', views.add_report, name='add-report'),
 ]
